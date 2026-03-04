3ff0bb0f714ebce8813f73069714352099e8049c
Phitchayapa <phitchayapa.chuc@bumail.net>
2026-03-03 16:00:08 -0800

    Add Movement script into the player

---
+++ Assets/Materials/Script/Movement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0f, moveZ);
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
